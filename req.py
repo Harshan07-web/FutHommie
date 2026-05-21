acbc6fcae2b81d534b65fb905d9c08acef3563c5
Harshan07-web <bharshan17@gmail.com>
2026-05-21 22:58:11 +0530

    extract teams from raw json

--- req.py
+++ req.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 #39	7293
@@ -8,7 +9,7 @@ load_dotenv()
 API = os.getenv("FOOTBALL_API")
 url = os.getenv("BASE_URL")
 
-def fetch_match(league_id : int,season : int):
+def fetch_fixtures(league_id : int,season : int):
     params = {
         "league" : league_id,
         "season" : season
@@ -22,4 +23,17 @@ def fetch_match(league_id : int,season : int):
 
     return response
 
-fetch_match(39,2024)
+json_data = fetch_fixtures(39,2024).json()
+
+
+
+tot_matches = json_data["results"]
+print(tot_matches)
+
+for i in range(tot_matches):
+    print(f"{json_data['response'][i]['teams']['home']['name']}")
+    print(f"{json_data['response'][i]['teams']['away']['name']}")
+    if json_data['response'][i]['teams']['home']['winner']:
+        print(f"Winner : {json_data['response'][i]['teams']['home']['name']}")
+    else:
+        print(f"Winner : {json_data['response'][i]['teams']['away']['name']}")
