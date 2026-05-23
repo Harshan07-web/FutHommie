850585538d5dd336934bf12449222281114b48a6
Harshan07-web <bharshan17@gmail.com>
2026-05-23 19:42:18 +0530

    Added logic to fetch data and turn into table

--- req.py
+++ req.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import json
+import pandas as pd
 
 load_dotenv()
 #39	7293
@@ -25,15 +26,68 @@ def fetch_fixtures(league_id : int,season : int):
 
 json_data = fetch_fixtures(39,2024).json()
 
+with open("raw_results.json","w") as f:
+    json.dump(json_data,f,indent=5)
 
 
 tot_matches = json_data["results"]
-print(tot_matches)
-
+final_data = {}
 for i in range(tot_matches):
-    print(f"{json_data['response'][i]['teams']['home']['name']}")
-    print(f"{json_data['response'][i]['teams']['away']['name']}")
-    if json_data['response'][i]['teams']['home']['winner']:
-        print(f"Winner : {json_data['response'][i]['teams']['home']['name']}")
+    home = json_data['response'][i]['teams']['home']['name']
+    away = json_data['response'][i]['teams']['away']['name']
+    home_goals = json_data['response'][i]['goals']['home']
+    away_goals = json_data['response'][i]['goals']['away']
+    if home not in final_data:
+        final_data[home] = {
+                            "wins" : 0, 
+                            "draws":0, 
+                            "losses":0,
+                            "goals":0,
+                            "goal con":0,
+                            "goal diff":0,
+                            "home_wins" :0, 
+                            "away_wins" :0 , 
+                            "points" : 0
+                            }
+    if away not in final_data:
+        final_data[away] = {
+                            "wins" : 0, 
+                            "draws":0, 
+                            "losses":0,
+                            "goals":0,
+                            "goal con":0,
+                            "goal diff":0, 
+                            "home_wins":0, 
+                            "away_wins" : 0, 
+                            "points" : 0
+                            }
+
+    final_data[home]['goals'] += home_goals
+    final_data[away]['goals'] += away_goals
+    final_data[home]["goal con"] += away_goals
+    final_data[away]["goal con"] += home_goals
+
+    home_win = json_data['response'][i]['teams']['home']['winner']
+    away_win = json_data['response'][i]['teams']['away']['winner']
+
+    if not home_win and not away_win:
+        final_data[home]["draws"] += 1
+        final_data[away]["draws"] +=1
+    elif home_win:
+        final_data[home]["wins"] += 1
+        final_data[home]["home_wins"] += 1
+        final_data[away]["losses"] +=1
     else:
-        print(f"Winner : {json_data['response'][i]['teams']['away']['name']}")
+        final_data[away]["wins"] += 1
+        final_data[away]["away_wins"] += 1
+        final_data[home]["losses"] +=1
+
+for team in final_data:
+    final_data[team]['points'] = final_data[team]['wins']*3 + final_data[team]['draws']*1
+    final_data[team]['goal diff'] = final_data[team]['goals'] - final_data[team]['goal con']
+table = pd.DataFrame.from_dict(final_data,orient='index')
+table.reset_index(inplace=True)
+table.rename(columns={"index" : "teams"}, inplace=True)
+table.sort_values("points",ascending=False,inplace=True)
+
+print(table)
