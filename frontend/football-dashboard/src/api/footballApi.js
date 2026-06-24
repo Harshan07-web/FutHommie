846065924c61100b433aecc76a51ab139b1701a9
Harshan07-web <bharshan17@gmail.com>
2026-06-24 16:32:40 +0530

    deployment

--- frontend/football-dashboard/src/api/footballApi.js
+++ frontend/football-dashboard/src/api/footballApi.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({
-    baseURL: "http://localhost:8000"
+export default axios.create({
+    baseURL: import.meta.env.VITE_API_URL
 });
-
-export default API;
